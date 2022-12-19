bf18d3a3f23eac9589b5cad74d7b24d450be6f59
Safonov Aleksandr <saff84@mail.ru>
2022-12-19 13:02:03 +0300

    v1.1

--- script/script.js
+++ script/script.js
@@ -1,37 +1,37 @@
 // document.write('Hello, world!');
 // console.log('Hello, developers!');
 
-let number = Math.floor(Math.random()*1000);
-console.log(number)
-let num2
+let number = Math.floor(Math.random() * 1000);
+console.log(number);
+let num2;
 
 while (number != num2) {
 
-num2 = prompt("Введите целое число или слово 'выход' для выхода")
-if (num2 == "выход"){
-    break
-}
+    num2 = prompt("Введите целое число или слово 'выход' для выхода")
+    if (num2 == "выход") {
+        break;
+    }
 
-else if (isNaN(num2)) {
-    alert(`Нужно ввести целое число ${num2}`)
-    console.log(`Нужно ввести целое число ${num2}`)
+    else if (isNaN(num2)) {
+        alert(`Нужно ввести целое число ${num2}`);
+        console.log(`Нужно ввести целое число ${num2}`);
 
-}
+    }
 
-else if (number == num2) {
-    alert(`"Поздравляю вы угадали!!!"`)
-    console.log(`"Поздравляю вы угадали!!!"`)
+    else if (number == num2) {
+        alert(`"Поздравляю вы угадали!!!"`);
+        console.log(`"Поздравляю вы угадали!!!"`);
 
-}
+    }
 
-else if (number > num2) {
-    alert(`Загаданное число больше ${num2}`)
-    console.log(`Загаданное число больше ${num2}`)
+    else if (number > num2) {
+        alert(`Загаданное число больше ${num2}`);
+        console.log(`Загаданное число больше ${num2}`);
 
-}
-else {
-    alert(`Загадонное число меньше ${num2}`)
-    console.log(`Загадонное число меньше ${num2}`)
+    }
+    else {
+        alert(`Загадонное число меньше ${num2}`);
+        console.log(`Загадонное число меньше ${num2}`);
 
-}
+    }
 }
